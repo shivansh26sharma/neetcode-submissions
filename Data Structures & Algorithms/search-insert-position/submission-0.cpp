8f82a26da1bad7d9aec15730ced18855175d6575
shivansh26sharma <86946366+shivansh26sharma@users.noreply.github.com>
2026-08-31 00:24:06 +0530

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
+        int i=0;
+        while(i<n){
+            if(nums[i]==target){
+                return i;
+            }
+            else if(nums[i]<target){
+                i++;
+            }
+            else{
+                return i;
+            }
+        }
+        return i;
+    }
+};
